07ddb3e7e2147f289cbba6d63ef531fadc6a9eef
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2023-10-19 17:27:08 -0400

    added queue to host room

--- app/host/[room]/page.tsx
+++ app/host/[room]/page.tsx
@@ -13,8 +13,8 @@ export default function Room({ params }: { params: { room: string } }) {
 
   useEffect(() => {
     socket.current = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || '');
-    socket.current.emit('ping');
-    socket.current.on('pong', () => {console.log("ponged")});
+    socket.current.emit('join', { room: params.room });
+    socket.current.on('pong', (data) => {console.log(data)});
 
     return () => {
       socket.current?.disconnect();
@@ -42,30 +42,52 @@ export default function Room({ params }: { params: { room: string } }) {
       image: "https://i.ytimg.com/vi/NCtzkaL2t_Y/maxresdefault.jpg",
       artist: "The Beatles 2"
     },
+    {
+      name: "The Beatles - Don't Let Me Down 2",
+      image: "https://i.ytimg.com/vi/NCtzkaL2t_Y/maxresdefault.jpg",
+      artist: "The Beatles 2"
+    },
+    {
+      name: "The Beatles - Don't Let Me Down 2",
+      image: "https://i.ytimg.com/vi/NCtzkaL2t_Y/maxresdefault.jpg",
+      artist: "The Beatles 2"
+    },
+    {
+      name: "The Beatles - Don't Let Me Down 2",
+      image: "https://i.ytimg.com/vi/NCtzkaL2t_Y/maxresdefault.jpg",
+      artist: "The Beatles 2"
+    },
+    {
+      name: "The Beatles - Don't Let Me Down 2",
+      image: "https://i.ytimg.com/vi/NCtzkaL2t_Y/maxresdefault.jpg",
+      artist: "The Beatles 2"
+    },
   ]
 
   return (
     <main className='flex justify-center items-center flex-col min-h-screen flex-none'>
-      <div className='grid grid-cols-4 gap-4 w-2/3'>
-        <Card className='col-span-3 p-4'>
-          <h1 className='font-bold text-2xl mb-2'>Room Queue:</h1>
-          <div className='overflow-y-scroll max-h-full'>
-            {
-              songs.map((song, i) => {
-                return (
-                  <SongCard
-                    name={song.name}
-                    image={song.image}
-                    artist={song.artist}
-                    key={i}
-                  />
-                )
-              })
-            }
-          </div>
-        </Card>
-        <div className='max-h-full'>
-          <Card className='flex items-center col-span-1 p-4'>
+      <div className='grid grid-cols-4 gap-4 w-2/3 items-center'>
+        <div className='col-span-3'>
+          <Card className='p-4'>
+            <h1 className='font-bold text-2xl mb-2'>Room Queue:</h1>
+            <div className='overflow-y-scroll max-h-[80vh]'>
+              {
+                songs.map((song, i) => {
+                  return (
+                    <SongCard
+                      name={song.name}
+                      image={song.image}
+                      artist={song.artist}
+                      key={i}
+                    />
+                  )
+                })
+              }
+            </div>
+          </Card>
+        </div>
+        <div className='max-h-full col-span-1'>
+          <Card className='flex items-center p-4'>
             <div className='pb-2 text-xl font-bold'>
               Join Room
             </div>
